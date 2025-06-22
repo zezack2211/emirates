29c82abdadaaf09b68dc1ca5cddaa6003ad101a3
zezack2211 <67494339+zezack2211@users.noreply.github.com>
2025-06-22 12:17:17 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,11 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-      
-    if (app()->environment('production')) {
-        URL::forceScheme('https');
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
-
 }
-
-
