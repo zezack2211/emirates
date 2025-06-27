9a3037071117528e4aa79b238087f392444de151
zezack2211 <67494339+zezack2211@users.noreply.github.com>
2025-06-27 12:54:27 +0200

    Update Application.php

--- app/Livewire/Student/Application.php
+++ app/Livewire/Student/Application.php
@@ -78,9 +78,9 @@ class Application extends Component
             'relative_phone' => $this->relative_phone,
         ]);
 
-        $photoPath = $this->photo->store( 'public');
-$certificatePath = $this->certificate->store( 'public');
-$idCardPath = $this->identity_card->store('public');
+        $photoPath = $this->photo->store('documents' ,'public');
+$certificatePath = $this->certificate->store( 'documents' ,'public');
+$idCardPath = $this->identity_card->store('documents' ,'public');
 
 Documents::create([
     'application_id' => $application->id,
